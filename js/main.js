a2abc4f3be78c3af1b41301e9d445db4909ef2e3
drew <dmerriweather4@Drews-MacBook-Pro.local>
2016-08-25 22:36:51 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+/**
+ * Created by dmerriweather4 on 7/21/16.
+ */
+//var thirdRow = $('.third-left-container').height();
+//$('.third-right-container').css('height', thirdRow);
+
+
+$(document).ready(function(){
+    $('.img-info').click(function(){
+        $('.pop-up').toggleClass('is-visible')
+    });
+
+    $('.scroll-to').click(function(event){
+        event.preventDefault();
+        var link = $(this).attr("href");
+        $('html, body').animate({
+            scrollTop: $( $(link)).offset().top-50
+        }, 1500);
+        return false;
+
+    });
+});
